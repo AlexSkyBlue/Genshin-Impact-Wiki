76250a4e40778ec8d0a34ede77d8d4fd1acda44b
AlexSkyBlue <ale.parra@duocuc.cl>
2022-08-29 22:21:42 -0400

    Inicio Routing

---
+++ src/app/tablinks/tablinks-routing.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { TablinksPage } from './tablinks.page';
+
+const routes: Routes = [
+  {
+    path: 'tablinks',
+    component: TablinksPage,
+    children: [
+      {
+        path: 'home',
+        loadChildren: () => import('../home/home.module').then( m => m.HomePageModule)
+      },{
+        path: 'tablinks',
+        loadChildren: () => import('../tablinks/tablinks.module').then( m => m.TablinksPageModule)
+      },{
+        path: 'perfil',
+        loadChildren: () => import('../perfil/perfil.module').then( m => m.PerfilPageModule)
+      },{
+        path: "",
+        redirectTo: "tablinks/home",
+        pathMatch: "full"
+      }
+    ]
+ },
+ {
+  path: "",
+  redirectTo: "tablinks/home",
+  pathMatch: "full"
+}
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class TablinksPageRoutingModule {}
